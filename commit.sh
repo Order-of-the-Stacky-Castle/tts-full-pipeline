7b2b035c54b98d5c14234d081ac77f346f9aea93
Ben Nowak <bennnowak@gmail.com>
2019-01-24 01:16:00 -0500

    "Cleaning it all up"

---
+++ commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+msg=$1
+ghrepo=""
+hrrepo=""
+
+do_ghrepo(){
+  printf "GitHub being processed\n"
+  git add --all
+  git commit -m \""$msg\""
+  git push
+}
+
+
+while [[ ! "$msg" ]];do
+  echo "Please provide a commit message"
+  read msg
+done
+
+do_ghrepo
+
+printf "Commit script completed."
